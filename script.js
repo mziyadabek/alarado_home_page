912a28f06bcea4093f6c5bdf13964d8c5fc101c2
Magzhan Ziyadabek <mziyadabek@gmail.com>
2024-08-24 17:12:13 +0500

    added burger on mobile device

--- script.js
+++ script.js
@@ -32,3 +32,47 @@ document.getElementById("theme").addEventListener("click", () => {
 
     
 })
+
+document.getElementById("theme3").addEventListener("click", () => {
+    if(!check){
+        const root = document.documentElement;
+        root.style.setProperty('--white', '#111729');
+        root.style.setProperty('--h1c', '#fff')
+        root.style.setProperty('--black', '#fff')
+        root.style.setProperty('--gray', '#fff')
+        logo.src = "alarado-icon-homepage-dark.svg"
+        chThImg1.src = "Moon_fill.svg"
+        chThImg2.src = "sun_dark.svg"
+        chThImg2.classList.remove("theme2")
+        chThImg1.classList.add("theme2")
+        check = true
+    }else{
+        const root = document.documentElement;
+        root.style.setProperty('--white', '#fff');
+        root.style.setProperty('--h1c', '#223344')
+        root.style.setProperty('--black', '#000')
+        root.style.setProperty('--gray', '#909193')
+        logo.src =" alarado-icon-homepage.svg"
+        chThImg1.src = "Moon_fill_light.svg"
+        chThImg2.src = "Sun_fill.svg"
+        chThImg2.classList.add("theme2")
+        chThImg1.classList.remove("theme2")
+        check = false
+    }
+
+    
+})
+
+const hamMenu = document.getElementById("burger");
+
+let offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  offScreenMenu.classList.toggle("active");
+});
+
+const xmark = document.getElementById("ximg")
+
+xmark.addEventListener("click", () =>{
+    offScreenMenu.classList.remove("active");
+})
